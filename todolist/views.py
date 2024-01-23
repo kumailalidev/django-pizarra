be53b956dd65f6caf3112cf4ba327f20b8361ca4
kumailalidev <kumailalidev@gmail.com>
2024-01-23 18:38:42 +0500

    Add todolist.

--- todolist/views.py
+++ todolist/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from project.models import Project
+
+from .models import TodoList
+
+
+def add(request, project_id):
+    project = Project.objects.filter(created_by=request.user).get(pk=project_id)
+
+    if request.method == "POST":
+        name = request.POST.get("name", "")
+        description = request.POST.get("description", "")
+
+        if name:
+            TodoList.objects.create(
+                project=project,
+                name=name,
+                description=description,
+                created_by=request.user,
+            )
+
+            return redirect(f"/projects/{project_id}/")
+
+    return render(request, "todolist/add.html")
