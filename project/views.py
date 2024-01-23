134b49b15e658dae93ebb556927d1f526fc05a44
kumailalidev <kumailalidev@gmail.com>
2024-01-23 05:25:08 +0500

    Create Project.

--- project/views.py
+++ project/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 from .models import Project
 
@@ -8,3 +8,22 @@ def projects(request):
     projects = Project.objects.filter(created_by=request.user)
 
     return render(request, "project/projects.html", {"projects": projects})
+
+
+@login_required
+def add_project(request):
+    if request.method == "POST":
+        name = request.POST.get("name", "")
+        description = request.POST.get("description", "")
+
+        if name:
+            Project.objects.create(
+                name=name,
+                description=description,
+                created_by=request.user,
+            )
+            return redirect("/projects/")
+        else:
+            print("Not valid")
+
+    return render(request, "project/add.html")
